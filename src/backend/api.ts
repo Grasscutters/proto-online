887cea8c7eb6301f202c41515d36687ce0ea1d00
KingRainbow44 <kobedo11@gmail.com>
2024-12-02 02:00:22 -0500

    fix(api.ts): Change how base URL is derived

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -4,7 +4,7 @@
  * Resolves the base URL of the API.
  */
 export function baseUrl(): string {
-    return typeof window === "undefined"
-        ? `http://localhost:${process.env.PORT ?? 3000}`
-        : window.location.origin;
+    return process.env.NODE_ENV == "development" ?
+        `http://localhost:${process.env.PORT ?? 3000}` :
+        `${process.env.BASE_URL}`;
 }
