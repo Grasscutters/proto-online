319e316989758948a8b6ac25aa2698efc49d2014
KingRainbow44 <kobedo11@gmail.com>
2024-12-02 02:04:25 -0500

    fix(api.ts): Use `NEXT_PUBLIC` environment variable

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -4,7 +4,5 @@
  * Resolves the base URL of the API.
  */
 export function baseUrl(): string {
-    return process.env.NODE_ENV == "development" ?
-        `http://localhost:${process.env.PORT ?? 3000}` :
-        `${process.env.BASE_URL}`;
+    return `${process.env.NEXT_PUBLIC_BASE_URL}`;
 }
