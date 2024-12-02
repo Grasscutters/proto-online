604dc8e00ebf19f232b52e72da55a871cbf206d2
KingRainbow44 <kobedo11@gmail.com>
2024-12-02 02:11:54 -0500

    fix(next.config.ts): Set body size limit to 10mb

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,10 @@ export default {
     /* Refer to https://github.com/vercel/next.js/blob/canary/packages/next/src/server/config-shared.ts */
     sassOptions: {
         silenceDeprecations: ["legacy-js-api"],
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "10mb"
+        }
     }
 } satisfies NextConfig;
